af21c387678711e015b58a2d9fba0bd0e04179e8
Kingunok <114995588+Kingunok@users.noreply.github.com>
2025-02-21 23:34:21 +0530

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,9 @@
+BOT_TOKEN = "7070276147:AAH0JlNODIsCFpdG6Gef5aE_V4Au4ruuQow"
+OWNER_ID = 6697298553
+TELEGRAM_API = 28693882
+TELEGRAM_HASH = "bc1f4414271fdd69990edff73dc7adc9"
+DATABASE_URL = "mongodb+srv://fino:fino@cluster0.ko0stef.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+BASE_URL = "https://mastersitetext.onrender.com"
+BASE_URL_PORT = 8080
+UPSTREAM_REPO = "https://github.com/Kingunok/Str"
+UPSTREAM_BRANCH = "wzv3"
